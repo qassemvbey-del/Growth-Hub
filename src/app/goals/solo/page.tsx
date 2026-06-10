a66464021cd16c52de5a5c6b9201a3d179d86892
qassem1911-png <qassem1911@gmail.com>
2026-06-11 01:23:33 +0300

    docs: document capacity check purge in solo wrapper page

--- src/app/goals/solo/page.tsx
+++ src/app/goals/solo/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 // import MissionsPage from '@/app/missions/page'
+// NOTE: Capacity checks (usedSlots + newSlots > 9) have been commented out within the underlying MissionsPage component
 import MissionsPage from '@/app/goals/page'
 
 export default function SoloGoalsPage() {
